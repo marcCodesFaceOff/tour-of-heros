3862eed2e9e6e92b933aad88c4e0978a394d1b80
Marc Fowler <marc.fowler.email@gmail.com>
2023-05-13 13:46:44 -0400

    adds search, save and add

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -11,7 +11,10 @@ import { MessageService } from './message.service';
 export class HeroService {
 
   private heroesUrl = 'api/heroes';  // URL to web api
-
+  
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
   constructor(private http: HttpClient, private messageService: MessageService) { }
 
   private log(message: string) {
@@ -50,7 +53,12 @@ private handleError<T>(operation = 'operation', result?: T) {
 
   }
 
-  /** GET hero by id. Will 404 if id not found */
+  /** GET hero by id. Will 404 if id not found 
+   * getHero() constructs a request URL with the desired hero's id
+   * The server should respond with a single hero rather than an array of heroes
+   * getHero() returns an Observable<Hero>, which is an observable of Hero objects 
+   * rather than an observable of Hero arrays. 
+  */
   getHero(id: number): Observable<Hero> {
     const url = `${this.heroesUrl}/${id}`;
     return this.http.get<Hero>(url)
@@ -58,7 +66,53 @@ private handleError<T>(operation = 'operation', result?: T) {
       /* The RxJS tap() operator enables this ability by looking at the observable 
       values, doing something with those values, and passing them along. */
       tap(_ => this.log(`fetched hero id=${id}`)),
-      catchError(this.handleError<Hero>(`getHero id=${id}`))
-  );
-}
+      catchError(this.handleError<Hero>(`getHero id=${id}`)));
+  }
+
+    /* GET heroes whose name contains search term */
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      // if not search term, return empty hero array.
+      return of([]);
+    }
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap(x => x.length ?
+        this.log(`found heroes matching "${term}"`) :
+        this.log(`no heroes matching "${term}"`)),
+      catchError(this.handleError<Hero[]>('searchHeroes', [])));
+  }
+
+  /** PUT: update the hero on the server */
+  updateHero(hero: Hero): Observable<any> {
+    /* The HttpClient.put() method takes three parameters:
+     * The URL
+     * The data to update, which is the modified hero in this case
+     * Options
+     * */
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero')));
+  }
+
+  /** POST: add a new hero to the server */
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero')));
+  }
+
+  /** DELETE: delete the hero from the server 
+   * deleteHero() calls HttpClient.delete()
+   * The URL is the heroes resource URL plus the id of the hero to delete
+   * You don't send data as you did with put() and post()
+   * You still send the httpOptions
+  */
+  deleteHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero')));
+  }
+
 }
