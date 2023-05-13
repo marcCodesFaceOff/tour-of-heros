a657c9b96ec0293639c37801aabb99f168c07653
Marc Fowler <marc.fowler.email@gmail.com>
2023-05-12 22:45:50 -0400

    adds hhtp client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   declarations: [
     AppComponent,
